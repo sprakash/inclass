dd32561f5defde0c85bdccd1a81d85c064c65273
unknown <prakashnitza@gmail.com>
2021-08-11 10:34:40 +0530

    post works now

--- express-exercise/catapp.js
+++ express-exercise/catapp.js
@@ -31,6 +31,7 @@ const express = require('express');
 const app = express();
 const port = 3000; 
 
+app.use(express.json());
 // Print some info for every request
 app.use(function (req, res, next) {
     console.log('Got a request:', req.method, req.originalUrl);
