479b5fa61ba7c0779d8713768ec7710c1b562ba6
unknown <prakashnitza@gmail.com>
2021-08-11 10:08:16 +0530

    express exercise added

---
+++ express-exercise/catapp.js
@@ -0,0 +1,122 @@
+/*
+README! Here's how to install this little demo. Type the following
+lines into a terminal.
+
+NOTE: The lines starting with '#' are comments that describe
+what the commands are doing; don't type in the comments:
+
+# Create a project directory called 'catapp'
+mkdir catapp
+
+# Make that directory your current directory
+cd catapp
+
+# Place catapp.js in that directory
+
+# (In this example I'm copying it from my Mac desktop)
+cp /Users/rudolf/Desktop/catapp.js catapp.js
+
+# Initialize npm and accept the defaults
+npm init -y
+
+# Install Express
+npm install express
+
+# Start your cat server!
+node catapp.js
+*/
+
+
+const express = require('express');
+const app = express();
+const port = 3000; 
+
+// Print some info for every request
+app.use(function (req, res, next) {
+    console.log('Got a request:', req.method, req.originalUrl);
+    next();  // give next middleware a chance to run
+});
+
+//start server
+app.listen(port, () => {
+    console.log(`catapp.js listening on port http://localhost:${port}`);
+});
+
+/**********************************************************
+ * My "Database"
+ **********************************************************/
+
+ let catsData = [
+    {id: 1, name: 'Fifi', breed: 'Calico', favoriteFood: 'tuna'},
+    {id: 2, name: 'Maxwell', breed: 'Red Tabby', favoriteFood: 'Swiss cheese'}
+];
+let nextId = 3;
+
+/**********************************************************
+ * Middleware
+ **********************************************************/
+
+
+
+
+/**********************************************************
+ * Routes
+ **********************************************************/
+
+// Get home page
+app.get('/', (req, res) => {
+    res.send('Hello Welcome to the World of CATS!!!');
+})
+
+// Get all cats
+app.get('/cats', (req, res) => {
+    res.send(catsData);
+});
+
+// Get a cat by ID
+app.get('/cats/:id', (req, res) => {
+    let id = Number(req.params.id);
+    let cat = catsData.find((c) => c.id === id);
+    if (cat) {
+        res.send({cat: cat});
+    } else {
+        res.status(404).send({error: "We don't have that cat here "});
+    }
+});
+
+// Add a new cat
+app.post('/cats', (req, res) => {
+    let newCat = req.body;
+    console.log(req);
+    newCat.id = nextId++;  // add a unique ID
+    catsData.push(newCat);
+    res.status(201).send({cat: newCat});
+});
+
+// Update/replace a cat by ID
+app.put('/cats/:id', (req, res) => {
+    let id = Number(req.params.id);
+    let ix = catsData.findIndex((c) => c.id === id);
+    if (ix === -1) {
+        res.status(404).send({error: 'Not Found'});
+    } else {
+        let modifiedCat = req.body;
+        modifiedCat.id = id;  // in case client tries to change the ID... bad!
+        catsData[ix] = modifiedCat;
+        res.send({cat: modifiedCat});
+    }
+});
+
+// Delete a cat by ID
+app.delete('/cats/:id', (req, res) => {
+    let id = Number(req.params.id);
+    let ix = catsData.findIndex((c) => c.id === id);
+    if (ix === -1) {
+        res.status(404).send({error: 'Not Found'});
+    } else {
+        catsData.splice(ix, 1);
+        res.send({message: 'Deleted'});
+    }
+});
+
+
